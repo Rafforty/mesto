e624907ced6550eb550051e298787d94fd844ec0
Savva Mishin <irafforty@yandex.ru>
2022-03-02 00:20:52 +0500

    fix after 1st review

--- README.md
+++ README.md
@@ -13,3 +13,7 @@
 **Figma**
 
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+
+**GH pages**
+
+*[Ссылка на проект на GH pages](https://rafforty.github.io/mesto/index.html)
---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+let popup = document.querySelector('.popup');
+let popupOpen = document.querySelector('.profile__edit');
+let popupClose = document.querySelector('.popup__close')
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_about');
+let jobInput = document.querySelector('.popup__input_job');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__job');
+
+function togglePopup () {   
+    popup.classList.toggle("popup_type_open");
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+    togglePopup (evt);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+popupOpen.addEventListener('click', togglePopup);
+popupClose.addEventListener('click', togglePopup);
